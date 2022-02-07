7f76d4a024496d3eff971f7ae83318653608e799
gehadaly <gehad.ali5555@gmail.com>
2022-02-08 00:54:24 +0200

    firstcommit

--- app/src/main/java/com/example/apptask/MainActivity.kt
+++ app/src/main/java/com/example/apptask/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.apptask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val itemlist=ArrayList<String>()
+
+    private lateinit var customAdapter:ADAPTER
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.resyclview)
+        itemlist.addAll(resources.getStringArray(R.array.names_item))
+        val recyclerView:RecyclerView=findViewById(R.id.recyclerView)
+        customAdapter=ADAPTER(itemlist)
+    val layoutManager=LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager=layoutManager
+        recyclerView.adapter=customAdapter
     }
 }
